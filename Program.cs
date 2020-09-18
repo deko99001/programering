a977a2fcaa8c5126a7b9c71fd2c3a2b54f77cdb3
deko99001 <70271729+deko99001@users.noreply.github.com>
2020-09-18 08:49:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = new int[5];
+            for (int i = 0; i < tal.Length; i++)
+            {
+                Console.Write("Ange tal " + (i + 1) + ": ");
+                tal[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 4; i >= 0; i--)
+            {
+                
+                Console.Write(tal[i]);
+            }
+        }
+    }
+}
